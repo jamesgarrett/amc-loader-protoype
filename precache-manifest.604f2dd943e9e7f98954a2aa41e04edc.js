d1342fc68c9a0be4672a051dde1e52ccf56855a7
jamesgarrett <jgsibinga@gmail.com>
2020-03-11 17:29:52 -0400

    Updates

---
+++ precache-manifest.604f2dd943e9e7f98954a2aa41e04edc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a125bfe66635a56f6bc1074aa749ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f129301d629c6bd5d6a",
+    "url": "/static/css/main.d40420df.chunk.css"
+  },
+  {
+    "revision": "612f7395e706df989331",
+    "url": "/static/js/2.ca1e68d4.chunk.js"
+  },
+  {
+    "revision": "1f129301d629c6bd5d6a",
+    "url": "/static/js/main.da97335d.chunk.js"
+  },
+  {
+    "revision": "d1877c0ff301cca38dd2",
+    "url": "/static/js/runtime~main.a0d9daf0.js"
+  }
+]);
